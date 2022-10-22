1e22cc7cc9c4d2cf7926476e112e98dfdba3cf6e
Pavel <rut100500@yandex.ru>
2022-10-22 22:30:45 +0300

    Домашнее задание сохр-1

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите два чиcла");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} равно числу {num2}");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите три чиcла");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine($"Самое большое число {num1}");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine($"Самое большое число {num2}");
+}
+else
+{
+    Console.WriteLine($"Самое большое число {num3}");
+}
+

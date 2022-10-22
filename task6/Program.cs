aeadf4d5195ce95f1f2c5e65d19e91d69381a030
Pavel <rut100500@yandex.ru>
2022-10-22 23:29:11 +0300

    Домашнее задание первого семинара_1

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 > 0)
+{
+    Console.WriteLine("Это число не является четным");
+}
+else
+{
+    Console.WriteLine("Это число является четным");
+}
---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i =2; i <= num; i= i+2)
+    { 
+        Console.Write(i);
+        Console.Write(",");
+    }
